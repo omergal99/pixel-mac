31d66766d310dd483a6c2d16c9b6d8e877ffda86
omergal99 <omergal99gm@gmail.com>
2019-06-06 18:07:19 +0300

    Updates

---
+++ precache-manifest.570846f9e672f59019ec66397c14f829.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cae1f07df5b3dd615507ee504a9125",
+    "url": "/pixel-mac/index.html"
+  },
+  {
+    "revision": "54e519ac4e0a44562e58",
+    "url": "/pixel-mac/static/css/main.7837d368.chunk.css"
+  },
+  {
+    "revision": "5fca2c286303de9d5954",
+    "url": "/pixel-mac/static/js/2.e2bc4550.chunk.js"
+  },
+  {
+    "revision": "54e519ac4e0a44562e58",
+    "url": "/pixel-mac/static/js/main.c1dd8fc0.chunk.js"
+  },
+  {
+    "revision": "aaf9befbd07b4be857d3",
+    "url": "/pixel-mac/static/js/runtime~main.be444ab6.js"
+  },
+  {
+    "revision": "18440e08823f8b121c95204a15b3a085",
+    "url": "/pixel-mac/static/media/LibreBaskerville-Regular.18440e08.ttf"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/pixel-mac/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "70fda92429e8a5f2fbaa5f10da118c74",
+    "url": "/pixel-mac/static/media/PT_Sans-Web-Regular.70fda924.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/pixel-mac/static/media/Quando-Regular.849e6795.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/pixel-mac/static/media/SourceSansPro-Regular.3f482321.ttf"
+  },
+  {
+    "revision": "0a956a90255e5c6772f56b918337ade5",
+    "url": "/pixel-mac/static/media/background.0a956a90.jpg"
+  }
+]);
